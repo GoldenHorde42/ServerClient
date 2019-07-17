f2afa97d3f7050f7ef80edde50c606b4c83570f9
goutham <gouthamswa@gmail.com>
2019-07-18 01:02:22 +0530

    basic edits

--- client.py
+++ client.py
@@ -15,19 +15,21 @@ response['operation'] = input()
 if response['operation'].lower() == 'get':
     print("ënter the name of the client who details you would like to see")
     response['data']['name'] = input ()
+    print(response)
     r1 = json.dumps(response)
     s.send(r1.encode())
-    r1 = s.recv(1024)
-    r1 = json.dumps(r1)
-    print(r1)
-
+    r2 = s.recv(1024)
+    print('Got: ', r2.decode())
 elif response['operation'].lower() == 'update':
         response['data']['info'] = input ()
         while response['data']['info'].lower () != 'bye':
    	        r1 = json.dumps(response)
+   	        print('Sending: ', r1)
    	        s.send(bytes(r1.encode()))
    	        print ("Type bye to end connection with server and anything else to continue conversation")
    	        response['data']['info'] = input()
 # Send 'bye' also to the server
+r1 = json.dumps(response)
+print('Sending bye: ', r1)
 s.send(bytes(r1.encode()))
 s.close()
--- server.py
+++ server.py
@@ -2,7 +2,9 @@ import sys
 import socket
 import threading
 import queue
+#import argparse
 import json
+
 
 # Global variables
 # TODO: Create a class to hold all the globals as statics
@@ -28,46 +30,65 @@ def serve():
            global lck,diction
         
        
-           localdiction = diction 
+           localdiction = diction
+           print(diction)
            localObj = Q.get()
-           name = localObj.recv(1024) 
+           name = localObj.recv(1024).decode ()
         
-           print("connected to ", str(name.decode()))        
-           response = localObj.recv(1024)
-           response = json.loads(response)
+           print("connected to ", str(name))
+           response_str = localObj.recv(1024).decode()
+           print("received: ", response_str)
+           response = json.loads(response_str)
+           print("after json loading: ", response)
            # Response should have an 'operation' field. Look for it.
            if 'operation' in response:
            # If operation says 'get' process accordingly.
+               print("Operation is: ", response['operation'])
                if response['operation'] == 'get':
                    # There should be a 'name' field inside
+                   print('Responding to get')
                    if 'name' in response['data']:
                        user_to_get = response['data']['name']
+                       print('Looking for user: ', user_to_get)
                        if user_to_get in localdiction:
-                           localObj.send(bytes(localdiction[user_to_get]))
+                           print('Located obj: ', localdiction[user_to_get])
+                           localObj.send(bytes(localdiction[user_to_get].encode()))
                        else:
+                           print('Could not get user: ', user_to_get)
                            localObj.send(bytes('N/A'.encode()))
-                   elif response['operation'] == 'update':
-                       
-                       lck.acquire()
-                       response = localObj.recv(1024).decode()
-                       r1 = json.loads(response)
-                       info = r1['data']['ínfo']
-                       while info.lower() != 'bye':
-                           localdiction[name] = info
-                           #if client send a bye then the server disconnects 
+               elif response['operation'] == 'update':
+                   print('Operation is: ', response['operation'])
+                   #lck.acquire()
+                   #print("still working")
+                   print ('Data is: ', response['data'])
+                   user_data = response['data']['info']
 
-                           response = localObj.recv(1024).decode()
-                           r1 = json.loads(response)
-                           info = response['data']['ínfo']
-                           writetofile(localdiction)
-                           diction = localdiction
-                           lck.release()
+                   while user_data.lower() != 'bye':
+                       print('user_data is: ', user_data)
+                       print('Operating record: ', name)
+                       localdiction[name] = user_data
+                       print(localdiction)
+                       #if client send a bye then the server disconnects
+
+                       resp_str = localObj.recv(1024).decode()
+                       print ('Received: ', resp_str)
+                       if resp_str == 'bye':
+                           break
+                       r1 = json.loads(resp_str)
+                       user_data = r1['data']['info']
+                       writetofile(localdiction)
+                       diction = localdiction
+                       #lck.release()
+                   print('Client said bye')
+               else:
+                   print('Invalid operation request: ', response['operation'])
     except socket.error as err:
         print("server is still closing try again in 30 seconds", err) 
 
 def main():     
     try:
-        
+
+        #argparse args(sys.argv)
         global diction,lck
         lck = threading.Lock ()
 
